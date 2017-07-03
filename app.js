8682c9b124938fbfdad166cc5792a8a3ae356065
Viktor Fomkin <v.fomkin@dvr.st>
2017-07-03 12:45:34 +0300

    commit

--- app.js
+++ app.js
@@ -37,7 +37,23 @@ app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({ extended: true }));//для работы с POST
 
 app.get('/', function (req, res) {
-    res.render('index', {title: 'API'})
+    res.render('index', {title: 'API', pages: pages});
+});
+
+
+app.get('/:page', function (req, res, next) {
+
+    var page = req.params.page;
+
+    if(page == 'blog/post'){
+            res.render('post', {pages: pages});
+    }
+    else if(page == 'blog') {
+        res.render('blog', {pages: pages});
+    }
+    else{
+        next();
+    }
 });
 
 app.get('/:id', function (req, res) {
@@ -46,49 +62,26 @@ app.get('/:id', function (req, res) {
         return page.id === req.params.id;
     });
     console.log(page.name);
-    res.render(page.name, {title: page.name, pages: pages})
+    res.render(page.name, {
+        title: page.name,
+        pages: pages,
+        page: page
+    });
 });
 
+/*
 app.get('/post', function (req, res) {
 
     // var blog = posts.find(function (link) {
     //     return blog.link === req.params.id;
     // });
     // console.log(blog.link);
-    res.render('/post'); 
-});
-
-
-/*app.get('/c', function (req, res) {
-    console.log(req.params);
-    // var course = courses.find(function (course) {
-    //     console.log(course.id);
-    //     return course.id === Number(req.params.id)
-    // });
-    // res.send(course);
-    res.end();
+    res.render('post');
 });*/
 
-
-/*app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connect', function(socket){
-    console.log('User connect');
-
-    socket.on('clientEvent', function(data){
-        console.log(data);
-    });
-
-    socket.on('disconnect', function(){
-        console.log('User disconnect');
-    });
-
-});*/
 
 // error handler
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -96,13 +89,17 @@ app.use(function(err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.render('error', {title: '404'});
+});*/
+
+
+app.use(function(req, res) {
+    res.status(404).render('error', {title: '404'});
+});
+
+app.use(function(req, res) {
+    res.status(500).render('error', {title: '500'});
 });
 
 http.listen(3001, function(){
-    // for (page in pages){
-    //     console.log(pages[page]);
-    // }
-
-
-    console.log('Yes it is');
+    console.log('Listen 3001 port');
 });
